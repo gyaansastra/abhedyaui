4bf054aa8fbc0b4fc855e34fa7ce417278e3c8a0
Debarghaya Dasgupta <35690123+gyaansastra@users.noreply.github.com>
2024-06-24 04:52:06 +1000

    Add files via upload

--- abhedya.sh
+++ abhedya.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-git clone 
+#Download the Instalation script
+wget https://github.com/gyaansastra/abhedyaui/blob/main/abhedya.sh
+
+#Convert the script to executable
+chmod +x abhedya.sh
